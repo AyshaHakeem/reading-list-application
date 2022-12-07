c04384617d22dcfa64297d50053efd5359a8e880
Aysha <ayshapp468@gmail.com>
2022-12-07 21:10:00 +0530

    Code to remove book from reading list

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,36 @@
 const form = document.querySelector('form')
-
-form.addEventListener('submit',(e)=>{
+const deleteBtn = document.querySelectorAll('.fa-trash')
+form.addEventListener('submit',()=>{
     document.querySelectorAll('.class').forEach(field =>{
         field.innerText='';
     })
 })
 
-// alert('working')
-// console.log('hello from main.js')
+deleteBtn.forEach(btn=>{
+    btn.addEventListener('click', deleteBook)
+})
+
+async function deleteBook(e){
+   let book = this.parentNode.childNodes[1].innerText
+   await fetch('/deleteBook', {
+      method: 'delete',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ 
+        'bookName' : book
+      })
+    })
+      .then(res=>{
+        if (res.ok)  {
+          return res.json()
+        }else{
+          console.error(res.statusText)
+        }
+        })
+      .then(data => {
+        window.location.reload()
+      })
+      .catch(err=>{
+        console.error(err)
+      })
+}
+  
--- server.js
+++ server.js
@@ -11,78 +11,55 @@ const url = process.env.DB_connectionString
 
 app.listen(process.env.port || PORT , ()=>{
     console.log('listening')
+})
+
     
-    // mongoose
-    mongoose.connect(url, { useNewUrlParser: true })
-    const db = mongoose.connection
-    db.once('open', result=>{
+// mongoose
+mongoose.connect(url, { useNewUrlParser: true })
+const db = mongoose.connection
+db.once('open', result=>{
+    console.log('connection established to db')
+})
+.on('error',err=>console.error(err))
 
-        // middle ware
-        app.use(express.static('public'))
-        app.use(bodyParser.urlencoded({ extended : true }))
-        app.use(bodyParser.json())
-        app.set('view engine', 'ejs')
+// middle ware
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended : true }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+
         
-        async function findBook(){
-            const b1 = await Book.find()
-            console.log(b1)
-        }
-        
-        app.get('/', async (req,res)=>{
+app.get('/', async (req,res)=>{
 
-            await Book.find()
-            .then(result => {
-                res.render('index.ejs', {result})
-            })
-            .catch()
-            
-        })
-
-        app.post('/addBook', async (req,response)=>{
-
-            let userInput = new Book({
-                name : req.body.bookName,
-                author: req.body.author
-            })
-
-            await userInput.save()
-            .then((result)=>{
-                console.log('Book added')
-                response.redirect('/')
-            })
-            .catch(err=> console.error(err))
-        })
-
+    await Book.find()
+    .then(result => {
+        res.render('index.ejs', {result})
     })
-    .on('error',err=>console.error(err))
+    .catch()
     
 })
 
-/*
+app.post('/addBook', async (req,response)=>{
+    let userInput = new Book({
+        name : req.body.bookName,
+        author: req.body.author
+    })
 
-// database// database
-    
-    db.once('open', ()=> {
-        console.log('Connected to Database: ' + url)
-        // middle ware
-        app.set('view engine', 'ejs')
-        app.use(express.static('public'))
-        app.use(bodyParser.urlencoded({ extended : true }))
-        app.use(bodyParser.json())
-       
+    await userInput.save()
+    .then(async (result)=>{
+        console.log('Book added')
+        response.redirect('/')
+    })
+    .catch(err=> console.error(err))
+})
 
-    })
-    db.on('error', e =>{
-        console.log('Connection error: '+ e )
-    })
+app.delete('/deleteBook',  async (req,res)=>{
+    const deleted = await Book.findOneAndDelete({name: req.body.bookName})
+    console.log('Book removed')
+    res.redirect('/')
+})
+
+
     
-    // schema
-    const b1 = new Book({
-        name: 'Tuesdays with Morrie',
-        author: 'Mitch Albom'
-    })
-    b1.save((err,doc)=>{
-        err? console.error(err) : console.log(doc)
-    })
-*/
+
 
